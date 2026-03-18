27232b132b48fa9619d1c821cf407280f08315d8
Jaden.IW <dkwano78@gmail.com>
2026-03-18 09:23:07 +0100

    YAHHHHHH

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-const $glowstone = document.getElementById("glowstone");
-const $light = document.getElementById("light");
-const $menu = document.getElementById("menu");
-const $submit = document.querySelector("#submit");
-const $span = document.querySelector("button");
-
-$light.addEventListener("click", () => {
-	$menu.showModal();
-});
-
-$span.addEventListener("click", () => {
-	document.getElementById("glowstone").src = "assets/glowstone-on.webp";
-
-	
-});
-
-function $span() {
-  const body = document.getElementById("myDIV").classList;
-  
-  body.remove("--body-light-off");
+function submit() {
+  const list = document.getElementById(".light-card").classList;
+  list.add(".light-card");
 }
